0d4157bca226ca8208cf3121d83e725f46a77df4
Regina König <koenig_regina@arcor.de>
2023-03-17 11:04:09 +0100

    Change data addr names

--- build64/debug_memtest.sh
+++ build64/debug_memtest.sh
@@ -125,7 +125,7 @@ Make() {
 # IMAGEBASE=$(grep -P '#define\tIMAGE_BASE' header.S | cut -f3)
 # BASEOFCODE=$(grep -P '#define\tBASE_OF_CODE' header.S | cut -f3)
 
-# TODO: get RELOCADDR and DATAADDR
+# TODO: get RELOCADDR and DATA
 # }
 
 Init() {
@@ -160,7 +160,7 @@ Init() {
         fi
 
         echo "add-symbol-file memtest.debug $OFFSET -s .data $DATAOFFSET" >> $GDB_FILE
-        echo "add-symbol-file memtest.debug $RELOCADDR -s .data $DATAOFFSET" >> $GDB_FILE
+        echo "add-symbol-file memtest.debug $RELOCADDR -s .data 0x423000" >> $GDB_FILE
 
         echo "b main" >> $GDB_FILE
         echo "commands" >> $GDB_FILE
